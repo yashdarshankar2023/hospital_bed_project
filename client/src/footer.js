f5419babdab5e2dde7e9cac0c2703b4acb373385
yourusername <email@youremail.com>
2022-10-12 09:10:44 +0530

    make it better

--- client/src/footer.js
+++ client/src/footer.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import "./App.css";
 
+
+
 const footer = () => {
+
   return (
+
     <div>
+
         <footer>
-        <p>this is footer</p>
+
+        <h2>Developed by</h2>
+        <p>YASH DARSHANKAR <br/>ROLL NO:- 02(1921271242002) </p>
+        <p>JAY PRAKASH PANDIT <br/>ROLL NO:- 18(1921271242018) </p>
+        
+        <a class="footer-link1" href="https://www.linkedin.com/feed/">LinkedIn</a>
+        <a class="footer-link2" href="https://www.instagram.com/?hl=en">Instagram</a>
+        <a class="footer-link2" href="https://www.facebook.com/">facebook</a>
+        <a class="footer-link2" href="mailto:s19_darshankar_yash@mgmcen.ac.in">CONTACT</a>
+        <p className="copy-right">© MGM COE NANDED</p>
+        
      </footer>
+
     </div>
+
   )
+
 }
+
+
 
 export default footer
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className='home'>
       <br /><br />
       <h1>This is a Home Page click below button for there uses</h1>
-      <br/><br/><br/>
+      <br /><br /><br />
       <div className='home_buttons'>
         <Link to="/data" className='home_link'><button className='home_btn'>Common Use</button></Link>
         <br />
@@ -14,6 +14,15 @@ const Home = () => {
 
 
       </div>
+      <br/>
+      <div className='home_desc'>
+        <div className='home_desc1'>this button is for common use, anyone can use it to see the current vacancy of hospitals</div>
+        <br />
+        <div className='home_desc1'>this button is only for hospital official only authenticated users should use it.</div>
+
+
+      </div>
+
       <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 
 
