ea455091dedb8ca390b56f9b02b85619a0c79bca
yourusername <email@youremail.com>
2023-01-07 12:33:17 +0530

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Data from './Data';
-import Login from './login';
-import Footer from './footer'
-import Navbar from './navbar';
-import Home from './pages/Home';
-import Pregister from './pages/prof_register';
+import Navbar from './pages/newnavbar';
+import Table from "./pages/table";
+import Details from './pages/details';
+import DetailsAdmin from './pages/details_admin';
+import Logs from "./pages/logs"
+import Admin from "./pages/admin"
+import Signup from "./pages/signup"
+import About from "./pages/about"
+
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -15,22 +20,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 export default function App1() {
+  
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar/>
       <Routes>
           
-          <Route path="/" element={<Home />} />
-   
-          <Route path="professional" element={<Login />} />
+          <Route path="/" element={<Table />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/logs" element={<Logs />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/details_admin" element={<DetailsAdmin />} />
+          
+          
+          
+          
+          
+          
 
-          <Route path="data" element={<Data />} />
-          <Route path="proff_register" element={<Pregister />} />
+          
+   
           
           
           
       </Routes>
-      <Footer/>
+      
     </BrowserRouter>
       
   );
